71f6ae13a87efb0670ad5777d60d8d8e4cc9162c
Liang Zhang <psychelzh@outlook.com>
2023-02-07 18:48:22 +0800

    Add g score pairs
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ _script_g_stability.R
@@ -0,0 +1,31 @@
+library(targets)
+tar_option_set(
+  packages = c("tidyverse", "lavaan", "conflicted"),
+  memory = "transient",
+  garbage_collection = TRUE,
+  error = "null",
+  format = "qs"
+)
+tar_source()
+future::plan(future.callr::callr)
+store_behav <- fs::path(
+  tar_config_get("store", project = "project_behav"),
+  "objects"
+)
+store_fmri <- fs::path(
+  tar_config_get("store", project = "project_task_fmri"),
+  "objects"
+)
+list(
+  tarchetypes::tar_file_read(
+    indices_wider_clean,
+    fs::path(store_behav, "indices_wider_clean"),
+    read = qs::qread(!!.x)
+  ),
+  tarchetypes::tar_file_read(
+    fc_data_rest_nn268_without,
+    fs::path(store_fmri, "fc_data_rest_nn268_without"),
+    read = qs::qread(!!.x)
+  ),
+  g_scores_pairs
+)
