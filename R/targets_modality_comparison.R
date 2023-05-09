2ed125ae61157bf0d086bdf493e2d29ef1d74630
Liang Zhang <psychelzh@outlook.com>
2023-05-09 15:46:27 +0800

    Add `project_modality_comparison`
    This is just to benchmark different modality prediction performance.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ R/targets_modality_comparison.R
@@ -0,0 +1,51 @@
+config_fc_data <- tidyr::expand_grid(
+  modal = c("Nbackrun1", "rest", "run1rest"),
+  parcel = c("nn268", "Power264"),
+  gsr = c("with", "without")
+)
+
+hypers_thresh <- dplyr::bind_rows(
+  tibble::tibble(
+    thresh_method = "alpha",
+    thresh_level = 0.01
+  ),
+  tibble::tibble(
+    thresh_method = "sparsity",
+    thresh_level = 0.01
+  )
+)
+
+modality_comparison <- tarchetypes::tar_map(
+  config_fc_data,
+  list(
+    tarchetypes::tar_file_read(
+      fc_data,
+      sprintf(
+        "data/neural/FC_modal-%s_parcel-%s_gsr-%s.arrow",
+        modal, parcel, gsr
+      ),
+      read = arrow::read_feather(!!.x)
+    ),
+    tarchetypes::tar_map_rep(
+      result_cpm,
+      command = behav_main |>
+        mutate(
+          cpm = map(
+            scores,
+            ~ do_cpm2(
+              fc_data,
+              .,
+              thresh_method = thresh_method,
+              thresh_level = thresh_level
+            )
+          ),
+          .keep = "unused"
+        ),
+      values = hypers_thresh,
+      batches = 4,
+      reps = 5
+    ),
+    tar_target(cpm_pred, extract_cpm_pred(result_cpm)),
+    tar_target(brain_mask, extract_brain_mask(result_cpm))
+  )
+)
---
+++ _script_modality_comparison.R
@@ -0,0 +1,45 @@
+library(targets)
+tar_option_set(
+  packages = c("tidyverse", "lavaan", "collapse"),
+  memory = "transient",
+  garbage_collection = TRUE,
+  storage = "worker",
+  retrieval = "worker",
+  error = "null",
+  format = "qs",
+  controller = crew::crew_controller_local(workers = 10)
+)
+tar_source()
+future::plan(future.callr::callr)
+
+store_behav <- fs::path(
+  tar_config_get("store", project = "project_behav"),
+  "objects"
+)
+store_g_invariance <- fs::path(
+  tar_config_get("store", project = "project_g_invariance"),
+  "objects"
+)
+
+list(
+  tarchetypes::tar_file_read(
+    behav_main,
+    fs::path(store_g_invariance, "behav_main"),
+    read = qs::qread(!!.x)
+  ),
+  tarchetypes::tar_file_read(
+    subjs_info_clean,
+    fs::path(store_behav, "subjs_info_clean"),
+    read = qs::qread(!!.x)
+  ),
+  modality_comparison,
+  tarchetypes::tar_combine(
+    cpm_pred,
+    modality_comparison$cpm_pred,
+    command = bind_rows(!!!.x, .id = "id") |>
+      clean_combined(
+        "cpm_pred",
+        c("num_vars", "id_pairs")
+      )
+  )
+)
