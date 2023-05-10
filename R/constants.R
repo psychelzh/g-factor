396fcd4f6db2058467fc4fe037bda578b93f07f5
Liang Zhang <psychelzh@outlook.com>
2023-05-10 22:47:17 +0800

    Added modality comparison
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ R/constants.R
@@ -0,0 +1,51 @@
+parcels <- c(
+  nn268 = "Shen268",
+  Power264 = "Power264"
+)
+scale_parcels <- list(
+  name = "Parcellation",
+  limits = names(parcels),
+  labels = parcels
+)
+
+thresh_methods <- c(
+  alpha = "Alpha",
+  sparsity = "Sparsity"
+)
+scale_thresh_methods <- list(
+  name = "Edge Threshold Method",
+  limits = names(thresh_methods),
+  labels = thresh_methods
+)
+
+edge_types <- c(
+  all = "Combined",
+  neg = "Anti-Corr Networks",
+  pos = "Pos-Corr Networks"
+)
+scale_edge_types <- list(
+  name = "Edge Type",
+  limits = names(edge_types),
+  labels = edge_types
+)
+
+modalities <- c(
+  run1rest = "Combined",
+  nbackfull = "Task",
+  rest = "Resting"
+)
+scale_modalities <- list(
+  name = "Modality",
+  limits = names(modalities),
+  labels = modalities
+)
+
+meas_behav <- c(
+  g_full = "G-Factor",
+  rapm = "RAPM"
+)
+scale_meas_behv <- list(
+  name = "Behavioral Measure",
+  limits = names(meas_behav),
+  labels = meas_behav
+)
--- R/utils-targets.R
+++ R/utils-targets.R
@@ -10,3 +10,20 @@ combine_targets <- function(name, targets, cols_targets) {
       substitute()
   )
 }
+
+tar_load_here <- function(name) {
+  name <- deparse1(substitute(name))
+  assign(
+    name,
+    tar_read_raw_here(name),
+    envir = parent.frame()
+  )
+}
+
+tar_read_here <- function(name) {
+  tar_read_raw_here(deparse1(substitute(name)))
+}
+
+tar_read_raw_here <- function(name) {
+  withr::with_dir(here::here(), targets::tar_read_raw(name))
+}
