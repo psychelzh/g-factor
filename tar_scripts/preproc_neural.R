252d423db536fab6abe62ec90b9b0ec2eedd98eb
Liang Zhang <psychelzh@outlook.com>
2023-06-23 16:18:16 +0800

    Remove site only covariates regression part
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tar_scripts/preproc_neural.R
+++ tar_scripts/preproc_neural.R
@@ -19,12 +19,6 @@ config_reg_covars <- config_file_tracking(
   tar_name_neural = "file_neural_reg_covars",
   name_suffix = "_reg_covars"
 )
-config_reg_site <- config_file_tracking(
-  config_neural,
-  dir_neural = "data/reg_site",
-  tar_name_neural = "file_neural_reg_site",
-  name_suffix = "_reg_site"
-)
 
 list(
   tarchetypes::tar_file_read(
@@ -49,26 +43,6 @@ list(
     values = dplyr::inner_join(
       config_origin,
       config_reg_covars,
-      by = names(config_neural)
-    )
-  ),
-  tarchetypes::tar_eval(
-    tar_target(
-      tar_neural_reg_site, {
-        arrow::read_feather(tar_neural) |>
-          regress_covariates(
-            subjs_covariates,
-            covars = "site",
-            cond = cond
-          ) |>
-          arrow::write_feather(file_reg_site, compression = FALSE)
-        file_reg_site
-      },
-      format = "file"
-    ),
-    values = dplyr::inner_join(
-      config_origin,
-      config_reg_site,
       by = names(config_neural)
     )
   )
