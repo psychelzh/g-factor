4f9a50b6d5f2e3b914c9efac1733a36f7c8886ad
Liang Zhang <psychelzh@outlook.com>
2023-06-30 01:14:56 +0800

    Enhance network visualization and analysis
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/visualize_network.R
+++ R/visualize_network.R
@@ -1,11 +1,8 @@
 #' Visualize brain mask as chord diagram
 #'
-#' @param adj_df A data frame contains adjacency matrix, should be the upper
-#'   triangle of the original full matrix.
+#' @param adj_mat A matrix of adjacency.
 #' @param roi_labels A data frame contains ROI labels and colors.
-#' @param link_val <[`tidy-select`][dplyr::dplyr_tidy_select]> Variable names
-#'   from `adj_df` used as the value of links. If `NULL`, the link value will be
-#'   set as the third column of `adj_df`.
+#' @param link_val A charact
 #' @param link_color A character string of length two, specifying the color of
 #'   the lowest and highest link value.
 #' @param group_by_hemi A logical value indicating whether to group ROIs by
@@ -13,32 +10,42 @@
 #' @returns Invisible `NULL`.
 #' @import circlize
 #' @export
-visualize_network <- function(adj_df, roi_labels,
-                              link_val = NULL,
+visualize_network <- function(adj_mat, roi_labels,
                               link_color = c("white", "black"),
                               group_by_hemi = TRUE) {
-  if (is.null(link_val)) link_val <- 3
   col_label <- if (group_by_hemi) "label_hemi" else "label"
   col_color <- if (group_by_hemi) "color_hemi" else "color_hex"
+
+  # get the degree
+  data_plot <- summarise_adjacency(adj_mat, roi_labels[[col_label]])
+
   # setup grid colors
   grid_colors <- roi_labels |>
     distinct(pick(all_of(c(col_label, col_color)))) |>
     deframe()
   if (group_by_hemi) {
     # extract hemisphere group names
-    label_hemis <- unique(roi_label[[col_label]])
+    labels_used <- data_plot |>
+      filter(val != 0) |>
+      distinct(x, y)
+    label_hemis <- factor(
+      union(labels_used$x, labels_used$y),
+      unique(roi_labels[[col_label]])
+    ) |>
+      sort()
     groups <- setNames(
       str_extract(label_hemis, "left|right"),
       label_hemis
     )
   }
+
   # plot chord diagram with groups support
   circos.clear()
   chordDiagram(
-    select(adj_df, 1, 2, {{ link_val }}),
+    data_plot,
     grid.col = grid_colors,
     col = colorRamp2(
-      range(adj_df[[3]]),
+      range(data_plot[[3]]),
       link_color,
       transparency = 0.5
     ),
@@ -114,7 +121,8 @@ prepare_roi_labels <- function(atlas, ...) {
       label_hemi = str_c(hemi, label, sep = "_")
     ) |>
     arrange(hemi, network) |>
-    mutate(label_hemi = as_factor(label_hemi))
+    mutate(label_hemi = as_factor(label_hemi)) |>
+    arrange(index)
 }
 
 #' Prepare adjacency matrix
@@ -146,4 +154,31 @@ vec_to_mat <- function(vec, diagonal = NA) {
   mat <- mat + t(mat)
   diag(mat) <- diagonal
   mat
+}
+
+summarise_adjacency <- function(adj_mat, labels,
+                                what = c("degree", "relative")) {
+  what <- match.arg(what)
+  adj_mat |>
+    as.data.frame() |>
+    rowid_to_column(var = "x") |>
+    pivot_longer(
+      -x,
+      names_to = "y",
+      values_to = "val"
+    ) |>
+    mutate(
+      y = parse_number(y)
+    ) |>
+    filter(x > y) |>
+    mutate(
+      across(
+        c(x, y),
+        ~ labels[.x]
+      )
+    ) |>
+    summarise(
+      val = sum(val),
+      .by = c(x, y)
+    )
 }
