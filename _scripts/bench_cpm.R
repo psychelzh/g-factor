6c576723e23f60286f218c6e7ea03a5ca9055e98
Liang Zhang <psychelzh@outlook.com>
2023-07-01 18:45:27 +0800

    Handle subjects pattern in preproc_neural project
    Also add a new file `file_subj_neural` to track neural subjects only.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/bench_cpm.R
+++ _scripts/bench_cpm.R
@@ -16,18 +16,6 @@ tar_option_set(
 tar_source()
 future::plan(future.callr::callr)
 
-# prepare static branches targets ----
-data_subjs <- tarchetypes::tar_map(
-  values = config_neural |>
-    config_file_tracking() |>
-    dplyr::distinct(cond, .keep_all = TRUE),
-  names = modal,
-  tar_target(
-    subjs_pattern,
-    arrow::read_feather(tar_neural)$sub_id
-  )
-)
-
 # targets pipeline ----
 list(
   tarchetypes::tar_file_read(
@@ -39,30 +27,6 @@ list(
     behav_main,
     fs::path(store_preproc_behav, "objects", "behav_main"),
     read = qs::qread(!!.x)
-  ),
-  data_subjs,
-  tarchetypes::tar_combine(
-    subjs_neural,
-    data_subjs$subjs_pattern,
-    command = list(!!!.x) |>
-      reduce(intersect)
-  ),
-  tar_target(
-    subjs_combined,
-    intersect(
-      subjs_neural,
-      behav_main |>
-        filter(idx == "g_full") |>
-        pluck("scores", 1, "sub_id")
-    )
-  ),
-  tar_target(
-    subjs_combined_file,
-    {
-      write_lines(subjs_combined, file_subjs_combined)
-      file_subjs_combined
-    },
-    format = "file"
   ),
   prepare_permute_cpm2(
     config_neural, hypers_cpm, behav_main,
--- _scripts/preproc_neural.R
+++ _scripts/preproc_neural.R
@@ -5,7 +5,7 @@ tar_option_set(
   packages = c("tidyverse", "lavaan"),
   memory = "transient",
   garbage_collection = TRUE,
-  error = "null",
+  error = "abridge",
   format = "qs",
   controller = crew::crew_controller_local(workers = 8)
 )
@@ -41,7 +41,24 @@ config_reg_covars_raw <- config_file_tracking(
   name_suffix = "_reg_covars_raw"
 )
 
+# prepare static branches targets ----
+data_subjs <- tarchetypes::tar_map(
+  values = config_origin |>
+    dplyr::distinct(cond, .keep_all = TRUE),
+  names = cond,
+  tar_target(
+    subjs_pattern,
+    arrow::read_feather(tar_neural)$sub_id
+  )
+)
+
+# targets pipeline ----
 list(
+  tarchetypes::tar_file_read(
+    behav_main,
+    fs::path(store_preproc_behav, "objects", "behav_main"),
+    read = qs::qread(!!.x)
+  ),
   tarchetypes::tar_file_read(
     subjs_covariates,
     fs::path(store_preproc_behav, "objects", "subjs_covariates"),
@@ -51,6 +68,38 @@ list(
     tar_target(tar_neural, file, format = "file_fast"),
     values = config_origin
   ),
+  data_subjs,
+  tarchetypes::tar_combine(
+    subjs_neural,
+    data_subjs$subjs_pattern,
+    command = list(!!!.x) |>
+      reduce(intersect)
+  ),
+  tar_target(
+    subjs_neural_file,
+    {
+      write(subjs_neural, file_subjs_neural, ncolumns = 1)
+      file_subjs_neural
+    },
+    format = "file"
+  ),
+  tar_target(
+    subjs_combined,
+    intersect(
+      subjs_neural,
+      behav_main |>
+        filter(idx == "g_full") |>
+        pluck("scores", 1, "sub_id")
+    )
+  ),
+  tar_target(
+    subjs_combined_file,
+    {
+      write(subjs_combined, file_subjs_combined, ncolumns = 1)
+      file_subjs_combined
+    },
+    format = "file"
+  ),
   tarchetypes::tar_eval(
     tar_target(
       tar_neural_reg_covars,
@@ -59,7 +108,7 @@ list(
         subjs_info = subjs_covariates,
         cond = cond
       ),
-      format = "file"
+      format = "file_fast"
     ),
     values = dplyr::inner_join(
       config_origin,
@@ -76,32 +125,11 @@ list(
         covars = c("age", "sex"),
         cond = cond
       ),
-      format = "file"
+      format = "file_fast"
     ),
     values = dplyr::inner_join(
       config_origin,
       config_reg_no_site,
-      by = names(config_neural)
-    )
-  ),
-  tarchetypes::tar_eval(
-    tar_target(tar_neural_raw, file_raw, format = "file_fast"),
-    values = config_origin_raw
-  ),
-  tarchetypes::tar_eval(
-    tar_target(
-      tar_neural_reg_covars_raw,
-      write_regressed_fc(
-        tar_neural_raw, file_reg_covars_raw,
-        subjs_info = subjs_covariates,
-        covars = c("age", "sex"),
-        cond = cond
-      ),
-      format = "file"
-    ),
-    values = dplyr::inner_join(
-      config_origin_raw,
-      config_reg_covars_raw,
       by = names(config_neural)
     )
   )
