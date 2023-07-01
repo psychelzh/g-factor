0cb7f91ee59df1b075968e4f6d4cbb047afc168b
Liang Zhang <psychelzh@outlook.com>
2023-07-01 20:06:46 +0800

    Add latent after gretna
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/latent_neural.R
+++ _scripts/latent_neural.R
@@ -14,6 +14,27 @@ tar_option_set(
 tar_source()
 future::plan(future.callr::callr)
 
+output_latent_fc <- function(files_in, file_out) {
+  data <- files_in |>
+    map(arrow::read_feather) |>
+    bind_rows(.id = "src")
+  reframe(
+    data,
+    across(
+      starts_with("Var"),
+      ~ cbind(.[src == 1], .[src == 2]) |>
+        princomp() |>
+        pluck("scores") |>
+        _[, 1]
+    )
+  ) |>
+    add_column(
+      sub_id = data$sub_id[data$src == 1],
+      .before = 1L
+    ) |>
+    write_feather_safely(file_out)
+}
+
 config_origin <- config_neural |>
   dplyr::filter(cond %in% c("nbackrun1", "rest")) |>
   config_file_tracking()
@@ -28,6 +49,29 @@ config_latent <- config_neural |>
   ) |>
   tidyr::chop(tar_neural)
 
+dir_gretna <- "data/neural-gretna"
+config_gretna <- config_neural |>
+  dplyr::filter(
+    cond %in% c("nbackrun1", "rest"),
+    filt == "bandpass"
+  ) |>
+  config_file_tracking(
+    dir_neural = dir_gretna,
+    tar_name_neural = "file_neural_gretna"
+  )
+config_latent_gretna <- config_neural |>
+  dplyr::filter(cond == "latent") |>
+  config_file_tracking(
+    dir_neural = dir_gretna,
+    name_suffix = "_latent"
+  ) |>
+  dplyr::select(-cond, -tar_neural_latent) |>
+  dplyr::inner_join(
+    dplyr::select(config_gretna, -cond, -file),
+    by = c("parcel", "filt", "gsr")
+  ) |>
+  tidyr::chop(tar_neural)
+
 list(
   tarchetypes::tar_eval(
     tar_target(tar_neural, file, format = "file_fast"),
@@ -36,28 +80,23 @@ list(
   tarchetypes::tar_map(
     values = config_latent,
     names = c(parcel, filt, gsr),
-    tar_target_raw(
-      "tar_neural_latent", rlang::expr({
-        data <- tar_neural |>
-          map(arrow::read_feather) |>
-          bind_rows(.id = "src")
-        reframe(
-          data,
-          across(
-            starts_with("Var"),
-            ~ cbind(.[src == 1], .[src == 2]) |>
-              princomp() |>
-              pluck("scores") |>
-              _[, 1]
-          )
-        ) |>
-          add_column(
-            sub_id = data$sub_id[data$src == 1],
-            .before = 1L
-          ) |>
-          write_feather_safely(file_latent)
-      }),
-      format = "file"
+    tar_target(
+      tar_latent,
+      output_latent_fc(tar_neural, file_latent),
+      format = "file_fast"
+    )
+  ),
+  tarchetypes::tar_eval(
+    tar_target(tar_neural, file, format = "file_fast"),
+    values = config_gretna
+  ),
+  tarchetypes::tar_map(
+    values = config_latent_gretna,
+    names = c(parcel, filt, gsr),
+    tar_target(
+      tar_latent_gretna,
+      output_latent_fc(tar_neural, file_latent),
+      format = "file_fast"
     )
   )
 )
