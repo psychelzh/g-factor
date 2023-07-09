a375ad8da2fe00e2af8beec7aee83acbf6a77175
Liang Zhang <psychelzh@outlook.com>
2023-07-09 10:15:37 +0800

    Merge neural pre-processing steps
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/preproc_neural.R
+++ _scripts/preproc_neural.R
@@ -24,7 +24,46 @@ write_regressed_fc <- function(origin, dest, ...) {
     write_feather_safely(dest)
 }
 
-config_regress <- config_file_tracking(config) |>
+output_latent_fc <- function(files_in, file_out) {
+  data <- files_in |>
+    map(arrow::read_feather) |>
+    bind_rows(.id = "src")
+  reframe(
+    data,
+    across(
+      starts_with("Var"),
+      ~ cbind(.[src == 1], .[src == 2]) |>
+        princomp() |>
+        pluck("scores") |>
+        _[, 1]
+    )
+  ) |>
+    add_column(
+      sub_id = data$sub_id[data$src == 1],
+      .before = 1L
+    ) |>
+    write_feather_safely(file_out)
+}
+
+config_files <- config_file_tracking(config)
+
+# latent construct
+config_latent <- config_files |>
+  dplyr::filter(
+    cond %in% c("nbackrun1", "rest", "latent"),
+    acq == "orig"
+  ) |>
+  dplyr::mutate(latent = ifelse(cond == "latent", "latent", "manifest")) |>
+  tidyr::pivot_wider(
+    id_cols = c(parcel, gsr, acq),
+    names_from = latent,
+    values_from = c(name, file),
+    values_fn = list
+  ) |>
+  tidyr::unnest(contains("latent"))
+
+# regress covariates
+config_regress <- config_files |>
   tidyr::pivot_wider(
     names_from = acq,
     values_from = c(file, name)
@@ -38,8 +77,19 @@ list(
     read = qs::qread(!!.x)
   ),
   tarchetypes::tar_eval(
-    tar_target(name_orig, file_orig, format = "file_fast"),
-    values = config_regress
+    tar_target(name, file, format = "file_fast"),
+    values = config_files |>
+      dplyr::filter(cond != "latent", acq != "reg")
+  ),
+  # TODO: https://github.com/ropensci/tarchetypes/discussions/153
+  tarchetypes::tar_map(
+    values = config_latent,
+    names = c(parcel, gsr, acq),
+    tar_target(
+      file_neural_latent,
+      output_latent_fc(name_manifest, file_latent),
+      format = "file_fast"
+    )
   ),
   tarchetypes::tar_eval(
     tar_target(
