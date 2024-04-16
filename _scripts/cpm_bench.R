12d4ba021a454a316015caacc82cd84d82445664
Liang Zhang <psychelzh@outlook.com>
2024-04-16 21:58:57 +0800

    Add spearman g to benchmarking
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/cpm_bench.R
+++ _scripts/cpm_bench.R
@@ -11,7 +11,7 @@ tar_option_set(
   format = "qs",
   controller = crew::crew_controller_local(
     name = "local",
-    workers = 8,
+    workers = 16,
     seconds_idle = 60
   )
 )
@@ -37,12 +37,50 @@ list(
     fs::path(store_preproc_behav, "objects", "scores_rapm"),
     read = qs::qread(!!.x)
   ),
+  tarchetypes::tar_file_read(
+    scores_spearman,
+    fs::path(store_preproc_behav, "objects", "scores_spearman"),
+    read = qs::qread(!!.x)
+  ),
   prepare_permute_cpm2(
-    dplyr::filter(config, acq == "reg"),
+    dplyr::filter(
+      config,
+      stringr::str_detect(cond, "eq") &
+        !stringr::str_detect(cond, "run1rest"),
+      acq == "reg",
+      parcel == "nn268"
+    ),
     hypers_cpm = hypers_cpm,
-    behav = tibble::tibble(scores = list(scores_rapm)),
+    behav = tibble::tibble(
+      index = c("spearman", "rapm"),
+      scores = list(scores_spearman, scores_rapm)
+    ),
     subjs_subset = subjs_combined,
     subjs_info = subjs_covariates,
     covars = c("age", "sex")
+  ),
+  prepare_permute_cpm2(
+    dplyr::filter(
+      config,
+      stringr::str_detect(cond, "eq") &
+        !stringr::str_detect(cond, "run1rest"),
+      acq == "reg",
+      parcel == "nn268",
+      gsr == "with"
+    ),
+    hypers_cpm = hypers_cpm |>
+      dplyr::filter(
+        thresh_method == "alpha",
+        thresh_level == 0.01
+      ),
+    behav = tibble::tibble(
+      index = c("spearman", "rapm"),
+      scores = list(scores_spearman, scores_rapm)
+    ),
+    subjs_subset = subjs_combined,
+    subjs_info = subjs_covariates,
+    covars = c("age", "sex", "site"),
+    include_file_targets = FALSE,
+    name_suffix = "reg_site"
   )
 )
